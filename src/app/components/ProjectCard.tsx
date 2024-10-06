d231ea8e2e56d22bf8812efa37a61233407eaa2a
Emeka-Okpaleke <olisemekaokpaleke@yahoo.com>
2024-10-06 17:57:38 +0100

    Icon added to ProjectCard

--- src/app/components/ProjectCard.tsx
+++ src/app/components/ProjectCard.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { CodeBracketIcon, EyeIcon } from '@heroicons/react/24/outline'
+import Link from 'next/link'
 
 const ProjectCard = ({imgUrl, title, description}) => {
   return (
@@ -6,7 +8,11 @@ const ProjectCard = ({imgUrl, title, description}) => {
         <div 
         className='h-52 md:h-72 rounded-t-xl'
         style={{ background: `url(${imgUrl})`, backgroundSize: "cover"}}
-        ></div>
+        >
+          <Link href="/" className='h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white'>
+            <CodeBracketIcon className='h-10 w-10 text-[#ADB7BE] m-2 absolute top-1/2 left-1/2 transform -translate-x-1/2 cursor-pointer hover:white'/>
+          </Link>
+        </div>
         <div className='text-white rounded-b-xl bg-[#181818] py-6 px-4'>
             <h5 className='text-xl font-semibold mb-2'>{title}</h5>
             <p className='text-[#ADB7BE]'>{description}</p>
