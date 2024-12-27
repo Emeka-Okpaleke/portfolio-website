144d4040621d35227394bd9efefb9b18b0511166
Emeka-Okpaleke <olisemekaokpaleke@yahoo.com>
2024-12-27 08:05:33 +0100

    typescript errorin emailsection fixed

--- src/app/api/send/route.tsx
+++ src/app/api/send/route.tsx
@@ -1,33 +1,10 @@
-// import type { NextApiRequest, NextApiResponse } from "next";
-// // import { EmailTemplate } from '../../components/EmailTemplate';
-// import { Resend } from "resend";
-
-// const resend = new Resend(process.env.RESEND_API_KEY);
-
-// export default async (req: NextApiRequest, res: NextApiResponse) => {
-//   const { data, error } = await resend.emails.send({
-//     from: "Emeka <olisemekaokpaleke@yahoo.com>",
-//     to: ["olisemekaokpaleke@yahoo.com"],
-//     subject: "Hello world",
-//     react: (
-//       <>
-//         <p>Email body</p>
-//       </>
-//     ),
-//   });
-
-//   if (error) {
-//     return res.status(400).json(error);
-//   }
-
-//   res.status(200).json(data);
-// };
+"use client";
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-const fromEmail = process.env.FROM_EMAIL;
+const fromEmail = process.env.FROM_EMAIL as string;
 
 export async function POST(req: NextRequest) {
   try {
